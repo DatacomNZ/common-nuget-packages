48ae9b6e2ff09e3c6246dd98208813792a1ee847
Troy Gorringe <troyg@datacom.co.nz>
2018-10-05 14:44:20 +1300

    SPIKE - Initial conceptual interfaces and controller for monitoring application health

---
+++ src/Datacom.Common.Diagnostics.WebApi/Api/Controllers/v0/HealthCheckController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Datacom.Common.Diagnostics.WebApi.Api.Controllers.v0
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/healthcheck")]
+    public class HealthCheckController : ApiController
+    {
+        private readonly List<ICheckAvailability> servicesToCheck;
+
+        public HealthCheckController(List<ICheckAvailability> servicesToCheck)
+        {
+            this.servicesToCheck = servicesToCheck;
+        }
+
+        [HttpGet]
+        [Route("isalive")]
+        public async Task<IHttpActionResult> IsAliveAsync()
+        {
+
+            var checksAndTasks = new List<(ICheckAvailability, Task<bool>)>(servicesToCheck.Count);
+
+            // Kick of tasks in parallel to perform checks
+            foreach (var serviceToCheck in servicesToCheck)
+            {
+                var task = serviceToCheck.CheckAccessAsync();
+                checksAndTasks.Add((serviceToCheck, task));
+            }
+
+            var isHealthy = true; // Happy, unless proven otherwise
+            foreach (var itemToCheck in checksAndTasks)
+            {
+                try
+                {
+                    var result = await itemToCheck.Item2;
+                    if (!result)
+                    {
+                        // Don't jump out early, it'll be nice to know what services are failing. What if two were failing?
+                        // TODO, uncomment, and provide any details
+                        //log.Error($"Access Check failed for {itemToCheck.Item1.GetLabel()}.");
+                        isHealthy = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // TODO - Uncomment, and provide details
+                    //log.Error(ex, $"");
+                    return InternalServerError(new Exception("Johny Five DIED"));
+                }
+
+            }
+
+            if (isHealthy)
+            {
+                return Ok("She'll be right");
+            }
+            return InternalServerError(new Exception("Johny Five DIED"));
+        }
+    }
+}
---
+++ src/Datacom.Common.Diagnostics/ICheckAvailability.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Datacom.Common.Diagnostics
+{
+    /// <summary>
+    /// Indicates the ability to check  health of service or component for a given system or components within the sytem
+    /// 
+    /// Example implementations might be a Datastore health checker, to ensure the database can be communicated with, and records can be read.
+    /// </summary>
+    public interface ICheckAvailability
+    {
+        /// <summary>
+        /// Check the ensure the service this represents can be accessed. E.g. records can be read from the database, or files from a file system service.
+        /// Intended to be polled frequently, so limt the extent and expense of the checks.
+        /// Note, this should be non destructive, and not affect any data
+        /// Can throw exceptions, as the consumer of this will explicitly handle exceptions
+        /// </summary>
+        /// <returns>true if happy happy joy joy, false if not</returns>
+        Task<bool> CheckAccessAsync();
+
+        /// <summary>
+        /// Performs more extensive and expensive checks across the  service. E.g. Ensure all key tables can be read via the ORM for a Database.
+        /// Note, this should be non destructive, and not affect any data
+        /// </summary>
+        /// <returns>true if all relevant items are accessible</returns>
+        Task<bool> CheckAccessExtensivelyAsync();
+
+        /// <summary>
+        /// A label describing the resource being checked
+        /// </summary>
+        /// <returns>A concise label (20 characters or less)</returns>
+        string GetLabel();
+
+        /// <summary>
+        /// Provide a list of warnings in relation to the service being monitored. E.g. for a filestore, the file system is nearing its capacity.
+        /// </summary>
+        /// <returns>Never null. Empty list in the case of no warnings.</returns>
+        Task<List<string>> GetWarningsAsync();
+
+
+        /// <summary>
+        /// Provide a list of errors in relation to the service being monitored. E>g. for a filestore, the filesystem
+        /// </summary>
+        /// <returns>Never null. Empty list in the case of no errors.</returns>
+        Task<List<string>> GetErrorsAsync();
+    }
+}
