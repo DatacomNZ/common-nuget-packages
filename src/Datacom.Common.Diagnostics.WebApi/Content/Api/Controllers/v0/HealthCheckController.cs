aad1e9f2db3e63fc597166d12bf2ef0cce4cb47e
Troy Gorringe <troyg@datacom.co.nz>
2018-10-09 15:31:00 +1300

    SPIKE - Upgrade to 4.6.2 project

--- src/Datacom.Common.Diagnostics.WebApi/Content/Api/Controllers/v0/HealthCheckController.cs
+++ src/Datacom.Common.Diagnostics.WebApi/Content/Api/Controllers/v0/HealthCheckController.cs
@@ -6,25 +6,24 @@ using System.Web.Http;
 namespace Datacom.Common.Diagnostics.WebApi.Api.Controllers.v0
 {
     [AllowAnonymous]
-    [RoutePrefix("api/healthcheck")]
+    [RoutePrefix("api/v0/healthcheck")]
     public class HealthCheckController : ApiController
     {
-        private readonly List<ICheckAvailability> servicesToCheck;
+        private readonly ICheckAvailability[] availabilityCheckers;
 
-        public HealthCheckController(List<ICheckAvailability> servicesToCheck)
+        public HealthCheckController(ICheckAvailability[] availabilityCheckers)
         {
-            this.servicesToCheck = servicesToCheck;
+            this.availabilityCheckers = availabilityCheckers;
         }
 
         [HttpGet]
         [Route("isalive")]
         public async Task<IHttpActionResult> IsAliveAsync()
         {
-
-            var checksAndTasks = new List<(ICheckAvailability, Task<bool>)>(servicesToCheck.Count);
+            var checksAndTasks = new List<(ICheckAvailability, Task<bool>)>(availabilityCheckers.Length);
 
             // Kick of tasks in parallel to perform checks
-            foreach (var serviceToCheck in servicesToCheck)
+            foreach (var serviceToCheck in availabilityCheckers)
             {
                 var task = serviceToCheck.CheckAccessAsync();
                 checksAndTasks.Add((serviceToCheck, task));
@@ -39,25 +38,23 @@ namespace Datacom.Common.Diagnostics.WebApi.Api.Controllers.v0
                     if (!result)
                     {
                         // Don't jump out early, it'll be nice to know what services are failing. What if two were failing?
-                        // TODO, uncomment, and provide any details
                         //log.Error($"Access Check failed for {itemToCheck.Item1.GetLabel()}.");
                         isHealthy = false;
                     }
                 }
                 catch (Exception ex)
                 {
-                    // TODO - Uncomment, and provide details
-                    //log.Error(ex, $"");
-                    return InternalServerError(new Exception("Johny Five DIED"));
+                    //log.Error(ex, $"Failed to complete health check");
+                    return InternalServerError(new Exception("Johny Five Segfaulted"));
                 }
 
             }
 
             if (isHealthy)
             {
-                return Ok("She'll be right");
+                return Ok("Johnny Five Alive");
             }
-            return InternalServerError(new Exception("Johny Five DIED"));
+            return InternalServerError(new Exception("Johny Five Segfaulted"));
         }
     }
 }
