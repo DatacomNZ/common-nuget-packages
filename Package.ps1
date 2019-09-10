c903bb670c55757bdb9d72d68a073625814cb7b6
Niall Duane <n_duane@hotmail.com>
2019-09-10 14:11:56 +1200

    Changed to powershell scripts. Added Azure-pipeline.

---
+++ Package.ps1
@@ -0,0 +1,17 @@
+param (
+    [string]$version  = "1.0.0"
+)
+
+$outputdirectory = "./output"
+
+dotnet build -c Release
+
+remove-item $outputdirectory -force -recurse -ErrorAction SilentlyContinue
+
+.\tools\nuget.exe pack "src/Datacom.Common/Datacom.Common.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
+.\tools\nuget.exe pack "src/Datacom.Common.Collections/Datacom.Common.Collections.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
+.\tools\nuget.exe pack "src/Datacom.Common.Diagnostics/Datacom.Common.Diagnostics.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
+.\tools\nuget.exe pack "src/Datacom.Common.Diagnostics.WebApi/Datacom.Common.Diagnostics.WebApi.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
+.\tools\nuget.exe pack "src/Datacom.Common.Mvc/Datacom.Common.Mvc.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
+.\tools\nuget.exe pack "src/Datacom.Common.SignalR/Datacom.Common.SignalR.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
+.\tools\nuget.exe pack "src/Datacom.Common.WebApi/Datacom.Common.WebApi.csproj" -OutputDirectory $outputdirectory -Version $version -properties Configuration=Release
